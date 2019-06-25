9111f39297aa0920d8bcbc53bd92b450afacdde0
xiaofandegeng <478465454@qq.com>
2019-06-25 19:51:20 +0800

    新增组件TodoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Hello,redux!
+            </div>
+        );
+    }
+}
+ 
+export default TodoList;
