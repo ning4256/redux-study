90bfd84bb1f7937fdd3f74510ee75af85f6c1c5e
xiaofandegeng <478465454@qq.com>
2019-06-26 21:49:27 +0800

    修改TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+// 数据
+const data = [
+    '早上开晨会',
+    '中午睡午觉',
+    '晚上敲代码'
+]
+
 
 class TodoList extends Component {
     constructor(props) {
@@ -7,9 +17,19 @@ class TodoList extends Component {
     }
     render() { 
         return ( 
-            <div>
-                Hello,redux!
-            </div>
+                <div style={{margin: '10px'}}>
+                    <div>
+                        <Input placeholder='Write something' style={{width: '250px'}}></Input>
+                        <Button type='danger'>增加</Button>
+                    </div>
+                    <div style={{margin: '10px', width: '300px'}}>
+                        <List
+                            bordered
+                            dataSource={data}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+                    </div>
+                </div>
         );
     }
 }
