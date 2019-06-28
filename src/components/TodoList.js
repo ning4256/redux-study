76ae67444c0335614a6279a0a711ba3447f9bcfd
xiaofandegeng <478465454@qq.com>
2019-06-28 08:24:22 +0800

    使用redux控制数据

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react'
+import store from '../store/index'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-
-// 数据
-const data = [
-    '早上开晨会',
-    '中午睡午觉',
-    '晚上敲代码'
-]
 
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        console.log(store.getState())
+        this.state = store.getState()
     }
     render() { 
         return ( 
                 <div style={{margin: '10px'}}>
                     <div>
-                        <Input placeholder='Write something' style={{width: '250px'}}></Input>
+                        <Input placeholder= {this.state.inputValue} style={{width: '250px'}}></Input>
                         <Button type='danger'>增加</Button>
                     </div>
                     <div style={{margin: '10px', width: '300px'}}>
                         <List
                             bordered
-                            dataSource={data}
+                            dataSource={this.state.listValue}
                             renderItem={item => (<List.Item>{item}</List.Item>)}
                         />
                     </div>
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue: '请输入相关内容',
+    listValue: [
+        '今天是周五',
+        '明天是周六',
+        '后天是周末'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}
